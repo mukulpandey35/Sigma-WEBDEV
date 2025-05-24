705a59dd5c30018eb8f89858a2f7ee9667b10388
mukulpandey35 <mukulrajapandey@gmail.com>
2025-05-25 01:25:55 +0530

    Video 54

---
+++ Video 54/script.js
@@ -0,0 +1,20 @@
+alert("Ha alert h ye");
+console.log("Yeh Value Console me dikhegi");
+
+prompt("Do you want to delete your number then enter");
+confirm("if okay then" );
+
+var a= prompt("Enter your name");
+console.log("Your name is "+a);
+
+var kyaTrueH = confirm("Okay ya cancel pr click kro");
+if(kyaTrueH){
+    console.log("Tumne Okay pe click kiya h")
+}
+else{
+    console.log("Tumne cancel pe click kiya h")
+}
+
+document.body.style.backgroundColor = "red";
+document.body.style.textAlign="Center";
+document.body.style.color="white";
